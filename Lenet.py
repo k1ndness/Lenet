9c39c4b0bb3e43a6b5689242c10ee4eabab4e2fa
k1ndness <dennis860106@gmail.com>
2022-04-07 20:15:02 +0800

    Lenet test

---
+++ Lenet.py
@@ -0,0 +1,142 @@
+import torch
+import torchvision
+import os
+import argparse
+import matplotlib.pyplot as plt
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.autograd import Variable
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
+
+
+# Training settings
+parser = argparse.ArgumentParser(description='PyTorch Lenet Example')
+parser.add_argument('--no_download_data', action='store_true', default=False,
+                    help='Do not download data')
+parser.add_argument('--batch-size', type=int, default=64, metavar='N',
+                    help='input batch size for training (default: 64)')
+parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
+                    help='input batch size for testing (default: 1000)')
+parser.add_argument('--epochs', type=int, default=10, metavar='N',
+                    help='number of epochs to train (default: 10)')
+parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
+                    help='learning rate (default: 0.01)')
+parser.add_argument('--momentum', type=float, default=0.5, metavar='M',
+                    help='SGD momentum (default: 0.5)')
+parser.add_argument('--no-cuda', action='store_true', default=False,
+                    help='enables CUDA training')
+parser.add_argument('--seed', type=int, default=1, metavar='S',
+                    help='random seed (default: 1)')
+parser.add_argument('--log-interval', type=int, default=200, metavar='N',
+                    help='how many batches to wait before logging training status')
+args = parser.parse_args()
+args.cuda = not args.no_cuda and torch.cuda.is_available()
+
+print(torch.cuda.is_available())
+print(args.cuda)
+
+torch.manual_seed(args.seed)
+if args.cuda:
+    torch.cuda.manual_seed(args.seed)
+
+print("Number of epochs: ", args.epochs)
+print("Batch size: ", args.batch_size)
+print("Log interval: ", args.log_interval)
+print("Learning rate: ", args.lr)
+
+train_data = torchvision.datasets.MNIST(
+    root='./mnist',
+    train=True,
+    transform=torchvision.transforms.ToTensor(), 
+    #把灰階從0~255壓縮到0~1
+    download=False
+)
+test_data = torchvision.datasets.MNIST(
+    root='./mnist/', 
+    train=False,
+    transform=torchvision.transforms.ToTensor(),
+    download=False,
+    )
+
+
+print(train_data.train_data.size())
+print(train_data.train_labels.size())
+
+plt.imshow(train_data.train_data[0].numpy(),cmap='gray')
+plt.title('%i' % train_data.train_labels[0])
+plt.show()
+
+
+train_loader = torch.utils.data.DataLoader(dataset = train_data, batch_size = args.batch_size, shuffle=True)
+test_loader = torch.utils.data.DataLoader(dataset = test_data, batch_size = args.batch_size, shuffle=True)
+
+class Lenet(nn.Module):
+    def __init__(self):
+        super(Lenet, self).__init__()
+        self.conv1 = nn.Conv2d(1, 6, kernel_size=5)
+        self.conv2 = nn.Conv2d(6, 16, kernel_size=5)
+        self.fc1 = nn.Linear(4*4*16, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = F.tanh(self.conv1(x))
+        x = F.max_pool2d(x,2,2)
+        x = F.tanh(self.conv2(x))
+        x = F.max_pool2d(x,2,2)    
+        x = x.view(-1,4*4*16)
+        x = F.tanh(self.fc1(x))
+        x = F.tanh(self.fc2(x))
+        x = F.softmax(self.fc3(x))
+        return x
+
+model = Lenet()
+if args.cuda:
+    model.cuda()
+
+optimizer = optim.SGD(model.parameters(),lr=args.lr,momentum=args.momentum)     
+def train(epoch):
+    model.train()
+    losses = []
+    for batch_idx, (data, target) in enumerate(train_loader):
+        if args.cuda:
+            data, target = data.cuda(), target.cuda()
+        data, target = Variable(data), Variable(target)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = F.cross_entropy(output, target)
+        loss.backward()
+        optimizer.step()
+        losses.append(loss.item())
+        if batch_idx % args.log_interval == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(epoch, batch_idx * len(data), len(train_loader.dataset),100. * batch_idx / len(train_loader), loss.item()))
+    print("\nTrain set: Average loss: {:.4f}".format(sum(losses) / len(losses)))
+
+def test(epoch):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    for data, target in test_loader:
+        if args.cuda:
+            data, target = data.cuda(), target.cuda()
+        data, target = Variable(data, volatile=True), Variable(target)
+        output = model(data)
+        test_loss += F.nll_loss(output, target).item()
+        pred = output.data.max(1)[1] # get the index of the max log-probability
+        correct += pred.eq(target.data).cpu().sum()
+
+    test_loss = test_loss
+    test_loss /= len(test_loader) # loss function already averages over batch size
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+
+
+for epoch in range(1, args.epochs + 1):
+    train(epoch)
+    test(epoch)
+
+
+
